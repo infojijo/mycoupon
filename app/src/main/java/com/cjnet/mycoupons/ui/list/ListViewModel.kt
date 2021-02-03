1f0a8ae895d9dd73ae80623a281c592b91cc40c7
Jijo Jose <info.jijo@gmail.com>
2021-02-03 15:05:21 -0500

    sqash again 3

---
+++ app/src/main/java/com/cjnet/mycoupons/ui/list/ListViewModel.kt
@@ -0,0 +1,13 @@
+package com.cjnet.mycoupons.ui.list
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Search List Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/cjnet/mycoupons/ui/map/MapFragment.kt
@@ -0,0 +1,31 @@
+package com.cjnet.mycoupons.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.cjnet.mycoupons.R
+
+class MapFragment : Fragment() {
+
+    private lateinit var homeViewModel: MapViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProviders.of(this).get(MapViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_map, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
