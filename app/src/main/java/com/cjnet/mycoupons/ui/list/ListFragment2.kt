a5a66d1232c26587e2e3e0d46c2c68e75c421c68
Jijo Jose <info.jijo@gmail.com>
2021-03-03 00:52:05 -0500

    my feature changes

---
+++ app/src/main/java/com/cjnet/mycoupons/ui/list/ListFragment2.kt
@@ -0,0 +1,58 @@
+package com.cjnet.mycoupons.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.cjnet.mycoupons.R
+
+class ListFragment2 : Fragment() {
+
+    private lateinit var dashboardViewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(ListViewModel::class.java)
+        //This method is just a sample.
+        val rootViewName = inflater.inflate(R.layout.fragment_list, container, false)
+        val textView: TextView = rootViewName.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+            //it here means this in old Java language
+            //even i have few changes to be done, here we go.
+            //in between that main branch pushed
+            //second line for rebase
+
+            //fixes made by other teams.
+
+            //feature 1
+
+            //feature 2
+            //this is a huge bug fix
+
+            //nice feature 11
+            //nice feature 12
+
+            //feature 1
+
+            //feature 2
+            //this is a huge bug fix
+
+            //nice bug fix 13
+
+            //tesla fix 01
+
+            //tesla fix 02
+            //tesla bug fixes in main #003
+        })
+        return rootViewName
+    }
+}
