de8b973b3bf3c96308d1ea65a5bd2fcac6e3d559
cjdev01 <mail.cjnet@gmail.com>
2021-03-03 00:46:15 -0500

    some feature commits goes here

---
+++ app/src/main/java/com/cjnet/mycoupons/MainActivity2.kt
@@ -0,0 +1,38 @@
+package com.cjnet.mycoupons
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        //b
+        //c
+        //d
+        //1
+        //2
+        //3
+        //4
+        //5
+        //6
+        //7
+        //8
+    }
+}
---
+++ app/src/main/java/com/cjnet/mycoupons/ui/map/MapFragment2.kt
@@ -0,0 +1,45 @@
+package com.cjnet.mycoupons.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.cjnet.mycoupons.R
+
+class MapFragment2 : Fragment() {
+
+    private lateinit var homeViewModel: MapViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProviders.of(this).get(MapViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_map, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+
+        //upwork feature 1
+
+        //upwork feature 2
+        //upwork main branch bug fixes
+
+        //upwork feature 3
+        //upwork feature 4
+
+        //upwork next major fixes
+
+        //11
+        //22
+        //33
+    }
+}
